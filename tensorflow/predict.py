e35f96dc3fc679d904eeb15aa261585976528aa5
Aslan Kartal <bilge.aslan@siemens.com>
2018-06-03 21:58:10 +0300

    Point Cloud (create ply file)

--- tensorflow/predict.py
+++ tensorflow/predict.py
@@ -2,8 +2,12 @@ import argparse
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+from plyfile import PlyData, PlyElement
 
 import models
 
@@ -50,7 +54,45 @@ def predict(model_data_path, image_path):
         plt.show()
         
         return pred
-        
+
+def create_point_Cloud(pred):
+    # Depth Intrinsic Parameters
+    fx_d = 5.8262448167737955e+02
+    fy_d = 5.8269103270988637e+02
+    cx_d = 3.1304475870804731e+02
+    cy_d = 2.3844389626620386e+02
+
+    imgDepthAbs = pred[0,:,:,:]
+    imgDepthAbs = cv2.resize(imgDepthAbs, (640,480))
+    print("imgDepthAbs shape: ", imgDepthAbs.shape)
+    [H, W] = imgDepthAbs.shape
+    assert H == 480
+    assert W == 640
+    
+    [xx, yy] = np.meshgrid(range(0, W), range(0, H))
+    X = (xx - cx_d) * imgDepthAbs / fx_d
+    Y = (yy - cy_d) * imgDepthAbs / fy_d
+    Z = imgDepthAbs
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+
+    # for c, m, zlow, zhigh in [('r', 'o', -50, -25)]:
+    #     ax.scatter(X.flatten(), Y.flatten(), Z.flatten(), c=Z.flatten(), cmap=plt.cm.coolwarm, marker=m)
+    
+    # ax.set_xlabel('X Label')
+    # ax.set_ylabel('Y Label')
+    # ax.set_zlabel('Z Label')            
+    # plt.show()
+
+    points = np.array([X.flatten(), Y.flatten(), Z.flatten()]).transpose()
+    points = [tuple(x) for x in points]
+    vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
+    print("Vertex shape: ", vertex.shape)
+
+    ply = PlyData([PlyElement.describe(vertex, 'vertex')], text=True)
+
+    ply.write('point_cloud.ply')   
                 
 def main():
     # Parse arguments
@@ -61,7 +103,8 @@ def main():
 
     # Predict the image
     pred = predict(args.model_path, args.image_paths)
-    
+    create_point_Cloud(pred)
+
     os._exit(0)
 
 if __name__ == '__main__':
